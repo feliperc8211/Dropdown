3ef2ffe55fc513d0054c811fe52800c10e8533a6
feliperc8211 <f.reyes8211@gmail.com>
2021-05-25 13:58:38 -0400

    subido

---
+++ src/components/Data.js
@@ -0,0 +1,13 @@
+export const options = [
+    { label: "Opcion 1", value: 1 },
+
+    { label: "Opcion 2", value: 2 },
+  
+    { label: "Opcion 3", value: 3 },
+  
+    { label: "Opcion 4", value: 4 },
+  
+    { label: "Opcion 5", value: 5 },
+  
+    { label: "Opcion 6", value: 6 }
+  ];
---
+++ src/components/Dropdown.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Lista from "./Lista";
+
+import { options } from './Data';
+
+class Dropdown extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      SelectedVal: ""
+    };
+  }
+
+  handleChange = e => {
+    var value = e.target.value;
+
+    this.setState({
+      SelectedVal: value
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Lista
+          id={"ddl1"}
+          name={"ddllocation"}
+          options={options}
+          title={"Opciones"}
+          handleChange={this.handleChange}
+          selectedValue={this.state.SeletedVal}
+        />
+      </div>
+    );
+  }
+}
+
+export default Dropdown;
